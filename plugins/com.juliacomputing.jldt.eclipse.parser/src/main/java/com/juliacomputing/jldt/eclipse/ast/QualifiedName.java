09981873c1415e99471cfb3ceed3d5cd2aa8a76a
snefru <taimur@btinternet.com>
2016-02-07 14:23:05 +0000

    Update manifests for minimum requirement

--- plugins/com.juliacomputing.jldt.eclipse.parser/src/main/java/com/juliacomputing/jldt/eclipse/ast/QualifiedName.java
+++ plugins/com.juliacomputing.jldt.eclipse.parser/src/main/java/com/juliacomputing/jldt/eclipse/ast/QualifiedName.java
@@ -4,13 +4,26 @@ import org.eclipse.dltk.ast.ASTVisitor;
 import org.eclipse.dltk.ast.references.SimpleReference;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 
 public class QualifiedName extends SimpleReference {
 
+    public static final String UNDEFINED = "undefined";
+
     public QualifiedName(List<String> entries, int start, int stop) {
-        super(start, stop, entries.stream().collect(Collectors.joining(".")));
+        super(start, stop, join(entries));
+    }
+
+    private static String join(final List<String> entries) {
+        final StringBuilder builder = new StringBuilder();
+        if (entries.isEmpty())
+            return UNDEFINED;
+        builder.append(entries.get(0));
+        for (int i = 1; i < entries.size(); i++) {
+            builder.append(".");
+            builder.append(entries.get(i));
+        }
+        return builder.toString();
     }
 
 
